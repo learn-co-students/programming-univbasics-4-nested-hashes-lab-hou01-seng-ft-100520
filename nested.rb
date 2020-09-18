3910ccbfd897eff3c4e26c2c5d6f121b33e55589
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-18 20:49:35 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,10 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
+
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -36,7 +38,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    "Object Orientation" 
+   
 end
 
 def dennis_ritchies_language
@@ -55,10 +58,12 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+    
+    "C"
 end
 
 def adding_matz
+  
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
@@ -80,8 +85,12 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       }
-    }
+    } 
+    programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
 
+programmer_hash
 end
 
 def changing_alan
@@ -103,7 +112,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
+programmer_hash[:alan_kay][:known_for] = "GUI"
 
+programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +136,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages] = "C", "Assembly"
+    programmer_hash
 end
